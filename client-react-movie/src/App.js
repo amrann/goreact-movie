cbf9b6e55351c5e7ec4683804b05f91ab78d90b3
meran <amr.bin.an@gmail.com>
2024-09-16 19:47:41 +0700

    integrasi bootstrap

--- client-react-movie/src/App.js
+++ client-react-movie/src/App.js
@@ -3,9 +3,111 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <div className='container'>
+				<div className='row'>
+					<h1 className='mt-3'>Go-React Movie Project!</h1>
+					<hr className='mb-3' />
+				</div>
+				<div className='row'>
+					<div className='col-sm-2'>
+            <div className='card'>
+              <ul className='list-group list-group-flush'>
+                <li className='list-group-item'>
+                  An Item
+                </li>
+                <li className='list-group-item'>
+                  A Second Item
+                </li>
+                <li className='list-group-item'>
+                  A Third Item
+                </li>
+              </ul>
+            </div>
+					</div>
+					<div className='col-sm-10'>
+          <div className="row">
+            <div className="col-sm-4 mb-2">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Special title treatment</h5>
+                  <p className="card-text">
+                    With supporting text below as a natural lead-in to additional content.
+                  </p>
+                  <a href="#" className="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4 mb-2">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Special title treatment</h5>
+                  <p className="card-text">
+                    With supporting text below as a natural lead-in to additional content.
+                  </p>
+                  <a href="#" className="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4 mb-2">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Special title treatment</h5>
+                  <p className="card-text">
+                    With supporting text below as a natural lead-in to additional content.
+                  </p>
+                  <a href="#" className="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4 mb-2">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Special title treatment</h5>
+                  <p className="card-text">
+                    With supporting text below as a natural lead-in to additional content.
+                  </p>
+                  <a href="#" className="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4 mb-2">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Special title treatment</h5>
+                  <p className="card-text">
+                    With supporting text below as a natural lead-in to additional content.
+                  </p>
+                  <a href="#" className="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4 mb-2">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Special title treatment</h5>
+                  <p className="card-text">
+                    With supporting text below as a natural lead-in to additional content.
+                  </p>
+                  <a href="#" className="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+					</div>
+				</div>
+			</div>
   );
 }
 

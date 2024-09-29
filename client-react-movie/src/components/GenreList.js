37e090bc1770cc8efead59497e2f81c4b241baea
meran <amr.bin.an@gmail.com>
2024-09-29 10:56:51 +0700

    display genres data in React

--- client-react-movie/src/components/GenreList.js
+++ client-react-movie/src/components/GenreList.js
@@ -1,32 +1,68 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function GenreList() {
   const [genres, setGenres] = useState([])
+  const [loaded, setLoaded] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+	const fetchGenres = async () => {
+		try {
+			const result = await axios(`http://localhost:4000/genres`);
+			await setGenres(result.data.genres);
+			setLoaded(true);
+		} catch (err) {
+			setErrorMessage(err.response.data);
+		}
+	};
+
 
   useEffect(() => {
-    setGenres([
-      {id: 1, name: 'Action'},
-      {id: 2, name: 'Comedy'},
-      {id: 3, name: 'Drama'},
-      {id: 4, name: 'Crime'},
-    ])
+    // // dummy data
+    // setGenres([
+    //   {id: 1, name: 'Action'},
+    //   {id: 2, name: 'Comedy'},
+    //   {id: 3, name: 'Drama'},
+    //   {id: 4, name: 'Crime'},
+    // ])
+    fetchGenres();
   }, [])
   
   return (
-    <div className='row'>
-      {genres.map((genre, index) => (
-        <div className='col-sm-2 mb-3' key={index}>
-          <div className='card'>
-            <div className='card-body text-center'>
-              <Link to={`/genres/${genre.id}/movies`}>
-                {genre.name}
-              </Link>
+    <>
+      {!loaded ? (
+				(() => {
+					if (errorMessage) {
+						return (
+							<div className='row'>
+								<p>Oops... {errorMessage}</p>
+							</div>
+						);
+					} else {
+						return (
+							<div className='row'>
+								<p>Loading...</p>
+							</div>
+						);
+					}
+				})()
+			) : (
+        <div className='row'>
+          {genres.map((genre, index) => (
+            <div className='col-sm-2 mb-3' key={index}>
+              <div className='card'>
+                <div className='card-body text-center'>
+                  <Link to={`/genres/${genre.id}/movies`}>
+                    {genre.genre_name}
+                  </Link>
+                </div>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
-      ))}
-    </div>
+      )}
+    </>
   )
 }
 
